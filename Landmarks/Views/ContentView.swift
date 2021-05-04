d7e7cd785a40675ed85dd4a4a984fd12eefdfdc6
Fernando Valverde <fernando@visualcosita.com>
2021-05-03 18:59:11 -0600

    Section 2

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Fernando Valverde on 3/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
